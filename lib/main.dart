172bb5620d2f4638d828ae38924743b63b5ed5f7
FlutLab.io <>
2022-09-21 12:38:59 +0000

    Created auth w/ pop-up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,15 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Flutter app',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'My app'),
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text('authorized'),
+        ),
+        body: Center());
   }
 }
 
@@ -26,16 +38,71 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final username = TextEditingController();
+    final password = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+          child: Padding(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Login',
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: TextFormField(
+                      controller: username,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter your username',
+                      ))),
+              TextFormField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your password',
+                  )),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: OutlinedButton(
+                  onPressed: () {
+                    if (username.text == 'app' && password.text == '12345') {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => MainPage(),
+                        ),
+                      );
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                          title: Text("Show Alert Dialog Box"),
+                          content: Text("You have raised a Alert Dialog Box"),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                              child: Text("Ok"),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Authorize'),
+                ),
+              )
+            ]),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+      )),
     );
   }
 }
